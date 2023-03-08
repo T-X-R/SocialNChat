e930c4d51fbc5ec8398d0712a08ae63672fe1f5d
T-X-R <xrtang213@gmail.com>
2023-03-07 21:21:15 -0500

    Create User Controller

---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+// create a new instance of the web application
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args); 
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
